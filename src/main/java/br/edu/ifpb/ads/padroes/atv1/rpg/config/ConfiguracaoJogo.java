98fef711195a908ade6bd6c743870b403aac0e74
Francisco-Lucas353 <FrancisLucasSM@gmail.com>
2026-06-10 20:59:13 -0300

    Update: transformando ConfiguracaoJogo em singleton

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/config/ConfiguracaoJogo.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/config/ConfiguracaoJogo.java
@@ -2,18 +2,21 @@ package br.edu.ifpb.ads.padroes.atv1.rpg.config;
 
 public class ConfiguracaoJogo {
 
+    private static ConfiguracaoJogo instancia;
     private int nivelDificuldade;
 
-    public ConfiguracaoJogo() {
+    // Construtor privado impede a criação externa com "new"
+    private ConfiguracaoJogo() {
         this.nivelDificuldade = 1;
     }
 
-    public int getNivelDificuldade() {
-        return nivelDificuldade;
+    public static ConfiguracaoJogo getInstancia() {
+        if (instancia == null) {
+            instancia = new ConfiguracaoJogo();
+        }
+        return instancia;
     }
 
-    public void setNivelDificuldade(int nivel) {
-        this.nivelDificuldade = nivel;
-    }
-
+    public int getNivelDificuldade() { return nivelDificuldade; }
+    public void setNivelDificuldade(int nivel) { this.nivelDificuldade = nivel; }
 }
