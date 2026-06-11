3d073ce7815eb7b8d88be309a1934ff2a60bae89
Francisco-Lucas353 <FrancisLucasSM@gmail.com>
2026-06-10 21:05:48 -0300

    Update: modificando o criador de personagens

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/CriadorPersonagem.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/CriadorPersonagem.java
@@ -4,133 +4,101 @@ public class CriadorPersonagem {
 
     public static Personagem criarPersonagem(String nome, String raca, String classe) {
 
-        int forca = 0, inteligencia = 0, agilidade = 0, vida = 0, mana = 0;
-        Arma arma = null;
-        Armadura armadura = null;
-        String[] habilidades = null;
+        Personagem.Builder builder = new Personagem.Builder().comIdentidade(nome, raca, classe);
+        boolean combinacaoValida = true;
 
         if (raca.equals("Humano")) {
             if (classe.equals("Guerreiro")) {
-                forca = 15;
-                inteligencia = 8;
-                agilidade = 10;
-                vida = 120;
-                mana = 30;
-
-                arma = new Arma("Espada de Ferro", 25, "Espada");
-                armadura = new Armadura("Armadura de Placas", 20, "Pesada");
-                habilidades = new String[]{"Investida", "Bloqueio"};
+                builder.comAtributos(15, 8, 10, 120, 30)
+                        .equipadoCom(new Arma("Espada de Ferro", 25, "Espada"),
+                                new Armadura("Armadura de Placas", 20, "Pesada"))
+                        .comHabilidades("Investida", "Bloqueio");
 
             } else if (classe.equals("Mago")) {
-                forca = 6;
-                inteligencia = 18;
-                agilidade = 8;
-                vida = 80;
-                mana = 150;
-                arma = new Arma("Cajado Mágico", 15, "Cajado");
-                armadura = new Armadura("Vestes Mágicas", 8, "Leve");
-                habilidades = new String[]{"Bola de Fogo", "Cura"};
+                builder.comAtributos(6, 18, 8, 80, 150)
+                        .equipadoCom(new Arma("Cajado Mágico", 15, "Cajado"),
+                                new Armadura("Vestes Mágicas", 8, "Leve"))
+                        .comHabilidades("Bola de Fogo", "Cura");
 
             } else if (classe.equals("Arqueiro")) {
-                forca = 10;
-                inteligencia = 12;
-                agilidade = 16;
-                vida = 100;
-                mana = 70;
-                arma = new Arma("Arco Élfico", 20, "Arco");
-                armadura = new Armadura("Armadura de Couro", 12, "Média");
-                habilidades = new String[]{"Tiro Certeiro", "Chuva de Flechas"};
-            }
+                builder.comAtributos(10, 12, 16, 100, 70)
+                        .equipadoCom(new Arma("Arco Élfico", 20, "Arco"),
+                                new Armadura("Armadura de Couro", 12, "Média"))
+                        .comHabilidades("Tiro Certeiro", "Chuva de Flechas");
+            } else { combinacaoValida = false; }
 
         } else if (raca.equals("Elfo")) {
             if (classe.equals("Guerreiro")) {
-                forca = 12;
-                inteligencia = 14;
-                agilidade = 16;
-                vida = 100;
-                mana = 60;
-                arma = new Arma("Lâmina Élfica", 22, "Espada");
-                armadura = new Armadura("Cota de Malha Élfica", 15, "Média");
-                habilidades = new String[]{"Dança das Lâminas", "Agilidade Élfica"};
+                builder.comAtributos(12, 14, 16, 100, 60)
+                        .equipadoCom(new Arma("Lâmina Élfica", 22, "Espada"),
+                                new Armadura("Cota de Malha Élfica", 15, "Média"))
+                        .comHabilidades("Dança das Lâminas", "Agilidade Élfica");
 
             } else if (classe.equals("Mago")) {
-                forca = 4;
-                inteligencia = 20;
-                agilidade = 14;
-                vida = 70;
-                mana = 180;
-                arma = new Arma("Cajado da Natureza", 18, "Cajado");
-                armadura = new Armadura("Mantos Élficos", 10, "Leve");
-                habilidades = new String[]{"Magia da Natureza", "Teleporte"};
+                builder.comAtributos(4, 20, 14, 70, 180)
+                        .equipadoCom(new Arma("Cajado da Natureza", 18, "Cajado"),
+                                new Armadura("Mantos Élficos", 10, "Leve"))
+                        .comHabilidades("Magia da Natureza", "Teleporte");
 
             } else if (classe.equals("Arqueiro")) {
-                forca = 8;
-                inteligencia = 16;
-                agilidade = 20;
-                vida = 90;
-                mana = 100;
-                arma = new Arma("Arco Longo Élfico", 28, "Arco");
-                armadura = new Armadura("Armadura de Couro Élfico", 14, "Média");
-                habilidades = new String[]{"Tiro Múltiplo", "Camuflagem"};
-            }
+                builder.comAtributos(8, 16, 20, 90, 100)
+                        .equipadoCom(new Arma("Arco Longo Élfico", 28, "Arco"),
+                                new Armadura("Armadura de Couro Élfico", 14, "Média"))
+                        .comHabilidades("Tiro Múltiplo", "Camuflagem");
+            } else { combinacaoValida = false; }
 
         } else if (raca.equals("Orc")) {
             if (classe.equals("Guerreiro")) {
-                forca = 20;
-                inteligencia = 6;
-                agilidade = 8;
-                vida = 150;
-                mana = 20;
-                arma = new Arma("Machado de Guerra", 30, "Machado");
-                armadura = new Armadura("Armadura Brutal", 25, "Pesada");
-                habilidades = new String[]{"Fúria", "Pancada Devastadora"};
+                builder.comAtributos(20, 6, 8, 150, 20)
+                        .equipadoCom(new Arma("Machado de Guerra", 30, "Machado"),
+                                new Armadura("Armadura Brutal", 25, "Pesada"))
+                        .comHabilidades("Fúria", "Pancada Devastadora");
 
             } else if (classe.equals("Mago")) {
-                // PROBLEMA: Combinação estranha, mas o código permite
-                forca = 10;
-                inteligencia = 14;
-                agilidade = 6;
-                vida = 100;
-                mana = 120;
-                arma = new Arma("Cajado Tribal", 12, "Cajado");
-                armadura = new Armadura("Vestes Xamânicas", 6, "Leve");
-                habilidades = new String[]{"Magia Sombria", "Invocação"};
+                builder.comAtributos(10, 14, 6, 100, 120)
+                        .equipadoCom(new Arma("Cajado Tribal", 12, "Cajado"),
+                                new Armadura("Vestes Xamânicas", 6, "Leve"))
+                        .comHabilidades("Magia Sombria", "Invocação");
 
             } else if (classe.equals("Arqueiro")) {
-                forca = 14;
-                inteligencia = 8;
-                agilidade = 12;
-                vida = 120;
-                mana = 40;
-                arma = new Arma("Arco de Osso", 24, "Arco");
-                armadura = new Armadura("Couro de Besta", 16, "Média");
-                habilidades = new String[]{"Tiro Brutal", "Intimidação"};
-            }
+                builder.comAtributos(14, 8, 12, 120, 40)
+                        .equipadoCom(new Arma("Arco de Osso", 24, "Arco"),
+                                new Armadura("Couro de Besta", 16, "Média"))
+                        .comHabilidades("Tiro Brutal", "Intimidação");
+            } else { combinacaoValida = false; }
+
+        } else {
+            combinacaoValida = false;
         }
 
-        // Retorna null se combinação inválida
-        if (arma == null) {
+        if (!combinacaoValida) {
             System.out.println("Combinação inválida: " + raca + " " + classe);
             return null;
         }
 
-        return new Personagem(nome, raca, classe, forca, inteligencia, agilidade,
-                vida, mana, arma, armadura, habilidades);
+        return builder.build();
     }
 
     public static Personagem criarPersonagemEspecial(String nome, String raca, String classe) {
+        // Aproveita o método acima para obter o modelo base correto
         Personagem base = criarPersonagem(nome, raca, classe);
         if (base == null) return null;
 
         if (raca.equals("Humano") && classe.equals("Guerreiro")) {
-            return new Personagem(nome + " o Lendário", raca, classe,
-                    18, 10, 12, 140, 40,
-                    new Arma("Excalibur", 35, "Espada"),
-                    new Armadura("Armadura do Rei", 30, "Pesada"),
-                    new String[]{"Investida", "Bloqueio", "Liderança"});
+
+            // Clona o personagem existente
+            Personagem lendario = base.clone();
+
+            lendario.setNome(nome + " o Lendário");
+            lendario.setForca(18);
+            lendario.setVida(140);
+            lendario.setArma(new Arma("Excalibur", 35, "Espada"));
+            lendario.setArmadura(new Armadura("Armadura do Rei", 30, "Pesada"));
+            lendario.setHabilidades(new String[]{"Investida", "Bloqueio", "Liderança"});
+
+            return lendario;
         }
 
         return base;
     }
-
 }
